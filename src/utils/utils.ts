07b464d341b09b30f2aef4a19ff97b69d69d01e5
Sam <quocdoansam.me@gmail.com>
2025-06-21 16:38:54 +0700

    Add formatDate utility function for date formatting

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,14 @@
 export const generateAvatarUrl = (seed: string) => {
   return `https://api.dicebear.com/9.x/dylan/svg?seed=${seed}`;
 };
+
+export const formatDate = (timestamp: number) => {
+  return new Date(timestamp * 1000).toLocaleString("en-GB", {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: false,
+  });
+};
