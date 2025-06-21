68b3eeda8d02506cb8e7374b1013f51bdac31521
Sam <quocdoansam.me@gmail.com>
2025-06-21 16:39:12 +0700

    Add route for CreatePage in the application

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,5 @@
 import MainLayout from "@/layouts/MainLayout";
+import CreatePage from "@/pages/CreatePage";
 import DashboardPage from "@/pages/DashboardPage";
 import ErrorPage from "@/pages/ErrorPage";
 import LoginPage from "@/pages/LoginPage";
@@ -7,7 +8,10 @@ export const routes = [
   {
     path: "",
     element: <MainLayout />,
-    children: [{ path: "", element: <DashboardPage /> }],
+    children: [
+      { path: "", element: <DashboardPage /> },
+      { path: "create", element: <CreatePage /> },
+    ],
   },
   { path: "/login", element: <LoginPage /> },
   { path: "*", element: <ErrorPage /> },
