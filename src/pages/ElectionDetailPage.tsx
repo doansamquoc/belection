958ce43393b892972c8eb7be13dc0acbf68763c4
Sam <quocdoansam.me@gmail.com>
2025-06-21 21:03:37 +0700

    Add ElectionDetailPage component to render ElectionDetail

---
+++ src/pages/ElectionDetailPage.tsx
@@ -0,0 +1,7 @@
+import ElectionDetail from "@/components/election/details/ElectionDetail";
+
+const ElectionDetailPage = () => {
+  return <ElectionDetail />;
+};
+
+export default ElectionDetailPage;
