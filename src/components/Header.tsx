b96d35555bec0ed40f29c8e8476e72774034c778
Sam <quocdoansam.me@gmail.com>
2025-06-21 14:44:05 +0700

    Implement Header component with dynamic navigation and logo display

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,30 @@
+import { House } from "lucide-react";
+import { Button } from "./ui/button";
+import { Link, useLocation } from "react-router-dom";
+import UserNavigation from "./navigations/UserNavigation";
+import Logo from "./Logo";
+
 const Header = () => {
-  return <div>Header</div>;
+  const location = useLocation();
+  const isDashboard =
+    location.pathname === "/" || location.pathname.startsWith("/page");
+
+  return (
+    <header className='border-b border-dashed'>
+      <div className='w-full max-w-4xl mx-auto flex justify-between border-x border-dashed p-4'>
+        {isDashboard ? (
+          <Logo />
+        ) : (
+          <Button variant={"default"} size={"icon"} asChild>
+            <Link to={"/"}>
+              <House />
+            </Link>
+          </Button>
+        )}
+        <UserNavigation />
+      </div>
+    </header>
+  );
 };
 
 export default Header;
