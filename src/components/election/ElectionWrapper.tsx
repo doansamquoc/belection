2f249d57b563d3b9370ff7f61a6ae187e8e85596
Sam <quocdoansam.me@gmail.com>
2025-06-21 16:38:19 +0700

    Add ElectionWrapper component to manage election display and loading state

---
+++ src/components/election/ElectionWrapper.tsx
@@ -0,0 +1,24 @@
+import type { ElectionSumaryType } from "@/types/ElectionSummaryType";
+import ElectionCard from "./ElectionCard";
+import ElectionCardSkeleton from "./skeletons/ElectionCardSkeleton";
+
+interface ElectionWrapperProps {
+  elections: ElectionSumaryType[];
+  isFetching: boolean;
+}
+
+const ElectionWrapper = ({ elections, isFetching }: ElectionWrapperProps) => {
+  return (
+    <div className='grid grid-cols-1 space-y-2'>
+      {isFetching
+        ? Array.from({ length: 4 }).map((_, i) => (
+            <ElectionCardSkeleton key={i} />
+          ))
+        : elections.map((election) => (
+            <ElectionCard election={election} key={election.createdAt} />
+          ))}
+    </div>
+  );
+};
+
+export default ElectionWrapper;
