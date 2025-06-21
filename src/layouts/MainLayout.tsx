1630dc5a7bb092bc845673af0f0e96a0d84f64dc
Sam <quocdoansam.me@gmail.com>
2025-06-21 19:36:09 +0700

    Refactor MainLayout component for improved layout and styling, and enhance background with grid pattern

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -4,10 +4,13 @@ import { Outlet } from "react-router-dom";
 
 const MainLayout = () => {
   return (
-    <div className='min-h-screen flex flex-col mx-auto'>
+    <div className='min-h-screen flex flex-col bg-gradient-to-b from-background to-background/95'>
       <Header />
-      <main className='flex flex-col flex-1 max-w-4xl w-full mx-auto border-x border-dashed p-4'>
-        <Outlet />
+      <main className='flex flex-col flex-1 max-w-4xl w-full mx-auto border-x border-border/30 p-4 relative'>
+        <div className='absolute inset-0 bg-grid-pattern opacity-[0.02] pointer-events-none' />
+        <div className='relative z-10'>
+          <Outlet />
+        </div>
       </main>
       <Footer />
     </div>
