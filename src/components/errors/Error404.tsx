b9c779741846b8a51a43c04c5026ec6953d92a35
Sam <quocdoansam.me@gmail.com>
2025-06-21 14:26:54 +0700

    Add Error404 component and ErrorPage for handling not found errors

---
+++ src/components/errors/Error404.tsx
@@ -0,0 +1,5 @@
+const Error404 = () => {
+  return <div>NOT FOUND!</div>;
+};
+
+export default Error404;
---
+++ src/pages/ErrorPage.tsx
@@ -0,0 +1,7 @@
+import Error404 from "@/components/errors/Error404";
+
+const ErrorPage = () => {
+  return <Error404 />;
+};
+
+export default ErrorPage;
