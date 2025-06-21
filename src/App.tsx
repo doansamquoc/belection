3d723a10bf898f40d5c37f7b215d8f7335475a70
Sam <quocdoansam.me@gmail.com>
2025-06-21 14:38:12 +0700

    Add ThemeProvider and ModeToggle components for theme management

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { ThemeProvider } from "./components/ThemeProvider";
 import AppRouter from "./routes/AppRouter";
 
 function App() {
-  return <AppRouter />;
+  return (
+    <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
+      <AppRouter />
+    </ThemeProvider>
+  );
 }
 
 export default App;
