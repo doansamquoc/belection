13170a944503a6e76c581cfd4d4503a6f5b16e17
Sam <quocdoansam.me@gmail.com>
2025-06-21 14:49:25 +0700

    Implement Footer component with layout and mode toggle functionality

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,19 @@
+import { ModeToggle } from "./ModeToggle";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className='border-t border-dashed'>
+      <div className='w-full max-w-4xl mx-auto flex justify-between items-center border-x border-dashed p-4'>
+        <span className='text-sm'>
+          Built by{" "}
+          <a href='https://github.com/doansamquoc' className='underline'>
+            Sam
+          </a>
+        </span>
+        <ModeToggle />
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
