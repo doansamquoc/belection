ba67cbc287a6a9a93b02a134212337d077023e18
Sam <quocdoansam.me@gmail.com>
2025-06-22 13:24:46 +0700

    Refactor utils to improve UUID generation and enhance date formatting; add address shortening function

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,8 +1,17 @@
+import { randomBytes, uuidV4 } from "ethers";
+
+export function generateUUID() {
+  return uuidV4(randomBytes(16));
+}
+
 export const generateAvatarUrl = (seed: string) => {
   return `https://api.dicebear.com/9.x/dylan/svg?seed=${seed}`;
 };
 
 export const formatDate = (timestamp: number) => {
+  if (timestamp === 0) {
+    return "No Deadline.";
+  }
   return new Date(timestamp * 1000).toLocaleString("en-GB", {
     day: "2-digit",
     month: "2-digit",
@@ -12,3 +21,7 @@ export const formatDate = (timestamp: number) => {
     hour12: false,
   });
 };
+
+export function shortenAddress(address: string, chars = 4) {
+  return `${address.slice(0, chars + 2)}...${address.slice(-chars)}`;
+}
